63f9705980dce693d493f48bd51324cc9fa9ab24
LonkarSneha <snehalonkar1@gmail.com>
2024-01-13 15:54:41 +0530

    [Sneha]:New Project

---
+++ src/eg1/Test.java
@@ -0,0 +1,33 @@
+package eg1;
+
+public class Test 
+{
+		public static void main(String[] args) 
+		
+		{
+			
+			// I want to store gender
+			// M --> male, F --> female
+			
+			//Use of char
+			
+			//1.Variable declaration
+			char gender;
+			
+			//2.Assign value to the variable
+			gender='M';
+			
+			//3.Usage
+			System.out.println("My gender is "+gender);
+			
+			
+			
+			//I want to store division
+			
+			char division;
+			division='A';
+			System.out.println("My division is "+division);
+			
+		}
+
+	}
